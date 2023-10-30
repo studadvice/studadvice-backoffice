227fd3d189cd7422ae10f879e25321ccc3402565
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-10-30 01:16:55 +0100

    feat[init](core) : Création des vues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/nav/navbar.component';
+import { HeaderComponent } from './core/nav/header/header.component';
+import { FooterComponent } from './core/nav/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
