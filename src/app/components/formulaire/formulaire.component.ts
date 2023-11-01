7f481de9ea1ba59ad39aa5f17af326b571a7ddf3
isidore amevigbe <isidore.amevigbe@worldline.com>
2023-11-02 00:05:30 +0100

    feat[formulaire] : création de la vue du formulaire de données

--- src/app/components/formulaire/formulaire.component.ts
+++ src/app/components/formulaire/formulaire.component.ts
@@ -9,10 +9,11 @@ import {FormControl} from "@angular/forms";
 export class FormulaireComponent {
     formControl = new FormControl();
     options: any[] = [
-        {label: 'Option 1', value: '1'},
-        {label: 'Option 2', value: '2'},
-        {label: 'Option 3', value: '3'},
-        {label: 'Option 4', value: '4'},
+        {value: 'Option 1', label: 'Option 1'},
+        {value: 'Option 2', label: 'Option 2'},
+        {value: 'Option 3', label: 'Option 3'},
+        {value: 'Option 4', label: 'Option 4'},
+        {value: 'Option 5', label: 'Option 5'},
     ];
 
 }
