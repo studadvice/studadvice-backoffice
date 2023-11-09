203cb76378c2c8770bd2da3b3fc40caa723b165e
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-11-09 01:04:21 +0100

    feat[modal] : Mise en place de l'ajout de la pagination
    DEVSTUD-14

--- src/app/core/pagination/pagination.component.ts
+++ src/app/core/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent {
+  @Input() totalItems!: number;
+  @Input() itemsPerPage!: number;
+  @Output() pageChanged = new EventEmitter<number>();
 
+  currentPage: number = 1;
+
+  constructor() { }
+
+  getTotalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  selectPage(page: number): void {
+    if (page < 1 || page > this.getTotalPages()) return;
+    this.currentPage = page;
+    this.pageChanged.emit(this.currentPage);
+  }
+
+  getPages(): Array<number> {
+    const totalPages = this.getTotalPages();
+    const pages: number[] = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
 }
