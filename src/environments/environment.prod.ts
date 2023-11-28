ff31eafd92bed5d73287a830b4b5ebf57dd5576d
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-11-28 02:32:20 +0100

    🌟 feat(back): Mise à jour effectué sur sur le fichier environment prod
    - 🆕 Modification de l'ajout de catégorie pour que cela correspond au model provenant du back
    
    🔖 Ticket associé : DEVSTUD-14

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAUuATt_IAe9MfQpOCGmSMItVQnNBJmJqQ",
+
+        authDomain: "eservices-clients.firebaseapp.com",
+
+        projectId: "eservices-clients",
+
+        databaseURL: "https://eservices-clients-default-rtdb.firebaseio.com/",
+        storageBucket: "eservices-clients.appspot.com",
+
+        messagingSenderId: "965275289761",
+
+        appId: "1:965275289761:web:69a66d3bff390db3ad48e0",
+
+        measurementId: "G-3CZY864992",
+    },
+    apiUrl: "http://codingschool-togo.com/api",
+    countryUrl: "https://restcountries.com/v3.1/all",
+    universityUrl: "http://universities.hipolabs.com/search?country=",
+    universityCountry: "France",
+};
