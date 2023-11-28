57079f542300de4b4b87f8aa7d545ffc4cafd8b0
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-11-28 12:39:49 +0100

    🌟 feat(back): Mise à jour effectué sur sur le fichier environment prod
    - 🆕 Modification de l'ajout de catégorie pour que cela correspond au model provenant du back
    
    🔖 Ticket associé : DEVSTUD-14

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,17 +23,3 @@ export const environment = {
     universityUrl: "http://universities.hipolabs.com/search?country=",
     universityCountry: "France",
 };
-
-// export const environment = {
-//     production: false,
-//     firebase: {
-//         apiKey: "AIzaSyAY-r8K78167ZcDHNmurip55HXTx7Izfd8",
-//         authDomain: "movie-app-aea22.firebaseapp.com",
-//         databaseURL: "https://movie-app-aea22-default-rtdb.europe-west1.firebasedatabase.app",
-//         projectId: "movie-app-aea22",
-//         storageBucket: "movie-app-aea22.appspot.com",
-//         messagingSenderId: "642920389814",
-//         appId: "1:642920389814:web:8f53319e914ddecd649a17",
-//         measurementId: "G-MD92DKE539"
-//     }
-// };
